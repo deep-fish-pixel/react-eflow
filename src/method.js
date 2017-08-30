0a3190e2a9cda1c34268358e30b7fdd8bd2405c0
mawei <120290590@qq.com>
2017-08-30 08:26:11 +0800

    调整getMethodName获取名称顺序

--- src/method.js
+++ src/method.js
@@ -2,13 +2,13 @@
 * 获取方法名称
 * */
 export function getMethodName(method) {
-  return method.name || method.displayName;
+  return method.displayName || method.name;
 }
 /*
  * 方法包含.符号时, 获取方法的最后非点的名称
  * */
 export function getOriginalMethodName(method) {
-  return (method.name || method.displayName).match(/[\w\_\-]+$/)[0];
+  return (method.displayName || method.name).match(/[\w\_\-]+$/)[0];
 }
 
 
