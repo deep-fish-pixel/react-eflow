139a8c2f0589d8057dc86e6a68f53772cc305a73
mawei <120290590@qq.com>
2017-09-06 06:46:39 +0800

    添加lib

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var babel = require('gulp-babel');
 //构建任务
 gulp.task('build', function(){
   //这里是将script文件下的js转换为ES5，并添加到dist文件夹中
-  gulp.src('src/*.js')
+  gulp.src('src/**/*.js')
     .pipe(babel({
       "plugins": ["transform-class-properties"],
       "presets": ["stage-0", "es2015", "react"]
